d5b1392874c7fb8d99e384c7baf87c100b10e2c4
dportnov <d.portnov@istock.info>
2023-07-31 18:07:27 +0300

    master

--- internal/api/home_page.go
+++ internal/api/home_page.go
@@ -9,13 +9,13 @@ import (
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Serving", r.Host, r.URL.Path)
 
-	temp, err := template.ParseGlob("./files/home.gohtml")
+	tmpl, err := template.ParseGlob("./files/home.gohtml")
 	if err != nil {
 		http.Error(w, fmt.Errorf("parse glob: %w", err).Error(), http.StatusBadGateway)
 		return
 	}
 
-	if err = temp.ExecuteTemplate(w, "home.gohtml", nil); err != nil {
+	if err = tmpl.ExecuteTemplate(w, "home.gohtml", nil); err != nil {
 		http.Error(w, fmt.Errorf("execute template: %w", err).Error(), http.StatusBadGateway)
 		return
 	}
